d3ec1d9e8c3051c033fc12571bbca140a4a4b45e
Ashley Bailey <admin@zeplinwarriors.com>
2025-03-01 18:44:58 +0000

    fix: 🐛 Missing Lang Func

--- apps/wizarr-frontend/src/modules/admin/pages/Invitations.vue
+++ apps/wizarr-frontend/src/modules/admin/pages/Invitations.vue
@@ -12,13 +12,17 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, getCurrentInstance } from "vue";
 import { useThemeStore } from "@/stores/theme";
 import { useModal } from "@/plugins/modal";
 
 import AdminTemplate from "@/templates/AdminTemplate.vue";
 import InvitationForm from "@/components/Forms/InvitationForm/InvitationForm.vue";
 import InvitationList from "../components/Invitations/InvitationList/InvitationList.vue";
+
+// Temporary fix for missing translations function
+const instance = getCurrentInstance();
+const __ = instance?.proxy?.__ ?? ((key: string) => key);
 
 const inviteModal = ref(false);
 const themeStore = useThemeStore();
