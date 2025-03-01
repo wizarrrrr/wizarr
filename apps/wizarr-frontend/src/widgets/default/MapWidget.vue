263c555e169e936896d480a1a662c54825e452bf
Ashley Bailey <admin@zeplinwarriors.com>
2025-03-01 18:35:17 +0000

    perf: 🚀 Use SVG map for better performance

--- apps/wizarr-frontend/src/widgets/default/MapWidget.vue
+++ apps/wizarr-frontend/src/widgets/default/MapWidget.vue
@@ -1,64 +1,49 @@
 <template>
-    <ol-map
-        ref="map"
-        :loadTilesWhileAnimating="true"
-        :loadTilesWhileInteracting="true"
-        class="absolute w-full h-full"
-    >
-        <ol-view
-            ref="view"
-            :center="center"
-            :rotation="rotation"
-            :zoom="zoom"
-            :projection="projection"
-        />
-
-        <ol-tile-layer>
-            <ol-source-xyz crossOrigin="anonymous" :url="mapTheme" />
-        </ol-tile-layer>
-    </ol-map>
+    <div id="map" class="absolute top-0 left-0 right-0 bottom-0 w-full h-full"></div>
 </template>
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import svgMap from "svgmap";
+import "svgmap/dist/svgMap.min.css";
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { useGeolocation } from '@vueuse/core';
-import { useThemeStore } from '@/stores/theme';
-import { mapState } from 'pinia';
-import { getTheme, type THEME } from '@/ts/utils/darkMode';
-
-export default defineComponent({
-    name: 'MapWidget',
-    data() {
-        return {
-            currentLocation: useGeolocation({ immediate: true }),
-            center: [40, 40],
-            rotation: 0,
-            zoom: 8,
-            projection: 'EPSG:4326',
-            mapTheme: '',
-            mapStyle: {
-                light: 'https://tile.jawg.io/jawg-streets/{z}/{x}/{y}.png?access-token=1VhtIV9KpAPP5AYqldYs0bIUI0Ap63FKA1NVLOMcrKHjFNljzfwg9dfkNkk6D8fg',
-                dark: 'https://tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=1VhtIV9KpAPP5AYqldYs0bIUI0Ap63FKA1NVLOMcrKHjFNljzfwg9dfkNkk6D8fg',
-            } as Record<THEME, string>,
-        };
-    },
-    computed: {
-        ...mapState(useThemeStore, ['theme']),
-    },
-    watch: {
-        'currentLocation.coords': {
-            immediate: true,
-            handler(coords: GeolocationCoordinates) {
-                this.center = [coords.longitude, coords.latitude];
+onMounted(() => {
+    new svgMap({
+        targetElementID: "map",
+        colorMin: "#A4CAFE",
+        colorMax: "#1A56DB",
+        flagType: "image",
+        flagURL: "https://flowbite.com/application-ui/demo/images/flags/{0}.svg",
+        data: {
+            data: {
+                visitors: {
+                    name: "Visitors:",
+                    format: "{0}",
+                    thousandSeparator: ",",
+                    thresholdMax: 500000,
+                    thresholdMin: 0,
+                },
+                change: {
+                    name: "Change:",
+                    format: "{0} %",
+                },
+            },
+            applyData: "visitors",
+            values: {
+                US: { visitors: 272109, change: 4.73 },
+                CA: { visitors: 160064, change: 11.09 },
+                DE: { visitors: 120048, change: -2.3 },
+                GB: { visitors: 110048, change: 3.3 },
+                FR: { visitors: 100048, change: 1.3 },
+                ES: { visitors: 90048, change: 1.5 },
+                JP: { visitors: 56022, change: 3.5 },
+                IT: { visitors: 48019, change: 1 },
+                NL: { visitors: 40016, change: 2 },
+                RU: { visitors: 30016, change: 3.4 },
+                CN: { visitors: 50016, change: 6 },
+                IN: { visitors: 140016, change: 2 },
+                BR: { visitors: 40016, change: 5 },
             },
         },
-        theme: {
-            immediate: true,
-            handler() {
-                const theme = getTheme(this.theme);
-                this.mapTheme = this.mapStyle[theme];
-            },
-        },
-    },
+    });
 });
 </script>
